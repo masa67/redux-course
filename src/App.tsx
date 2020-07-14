7a4662ca785608aa81a9916ec1f7cdd6f44b071e
Matti Sangi <adm.masaweb@gmail.com>
2020-07-14 11:10:20 +0300

    Lesson #2 changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,27 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to React with Redux.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="Todo-App">
+        <form>
+          <input type="text" />
+        </form>
+        <div className="Todo-List">
+          <ul>
+            <li>
+              <input type="checkbox" /> Create Static UI
+            </li>
+            <li>
+              <input type="checkbox" /> Create Initial State
+            </li>
+            <li>
+              <input type="checkbox" /> Use State to Render UI
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
