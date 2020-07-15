c5b433c6a3151827a5550abc8c3790378a8d7e39
Matti Sangi <adm.masaweb@gmail.com>
2020-07-15 09:48:28 +0300

    Lesson #7 changes: render react ui based on state changes with subscribe in redux

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,33 @@ import App from './App';
 import { Todo } from './components/TodoList';
 import * as serviceWorker from './serviceWorker';
 import store from './store'
+import { TodoActionType } from './reducers/todo';
 
-const state = store.getState()
+const render = () => {
+  const state = store.getState()
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App {...state}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  ReactDOM.render(
+    <React.StrictMode>
+      <App {...state} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+render()
+
+store.subscribe(render)
+
+setTimeout(() => {
+  store.dispatch({
+    type: TodoActionType.TODO_ADD,
+    payload: {
+      id: 4,
+      name: 'new todo',
+      isComplete: false
+    }
+  })
+}, 1000)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
