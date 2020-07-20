e048b8fd5c4544612d5978e2c40a99d2afa1a092
Matti Sangi <adm.masaweb@gmail.com>
2020-07-20 09:32:13 +0300

    Lesson #24 changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 import Message from './components/Message';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -14,11 +16,14 @@ function App() {
           Welcome to React with Redux.
         </p>
       </header>
-      <div className="Todo-App">
-        <Message />
-        <TodoForm />
-        <TodoList />
-      </div>
+      <Router>
+        <div className="Todo-App">
+          <Message />
+          <TodoForm />
+          <TodoList />
+          <Footer />
+        </div>
+      </Router>
     </div>
   );
 }
